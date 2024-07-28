8795d65455b39d299ab6da48fd87c535b19c527c
Arinobu Fukuhara <afukuhara@gmail.com>
2024-07-28 15:31:32 +0900

    Results for 2024/07/28

--- ch08_cut/cutr/src/mylib.rs
+++ ch08_cut/cutr/src/mylib.rs
@@ -1,13 +1,6 @@
 use crate::Extract::*;
 use clap::{App, Arg};
-use regex::Regex;
-use std::{
-    error::Error,
-    fs::File,
-    io::{self, BufRead, BufReader},
-    num::NonZeroUsize,
-    ops::Range,
-};
+use std::{error::Error, ops::Range};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 type PositionList = Vec<Range<usize>>;
@@ -36,15 +29,8 @@ pub fn get_args() -> MyResult<Config> {
                 .value_name("FILE")
                 .help("Input file(s)")
                 .multiple(true)
+                .required(true)
                 .default_value("-"),
-        )
-        .arg(
-            Arg::with_name("delimiter")
-                .value_name("DELEMITER")
-                .short("d")
-                .long("delim")
-                .help("Field delimiter")
-                .default_value("\t"),
         )
         .arg(
             Arg::with_name("bytes")
@@ -52,7 +38,11 @@ pub fn get_args() -> MyResult<Config> {
                 .short("b")
                 .long("bytes")
                 .help("Selected bytes")
-                .conflicts_with_all(&["chars", "fields"]),
+                .takes_value(true)
+                .multiple(true)
+                .conflicts_with("chars")
+                .conflicts_with("fields")
+                .validator(is_number),
         )
         .arg(
             Arg::with_name("chars")
@@ -60,7 +50,11 @@ pub fn get_args() -> MyResult<Config> {
                 .short("c")
                 .long("chars")
                 .help("Selected characters")
-                .conflicts_with_all(&["bytes", "fields"]),
+                .takes_value(true)
+                .conflicts_with("bytes")
+                .conflicts_with("fields")
+                .multiple(true)
+                .validator(is_number),
         )
         .arg(
             Arg::with_name("fields")
@@ -68,118 +62,131 @@ pub fn get_args() -> MyResult<Config> {
                 .short("f")
                 .long("fields")
                 .help("Selected fields")
-                .conflicts_with_all(&["bytes", "chars"]),
+                .takes_value(true)
+                .conflicts_with("chars")
+                .conflicts_with("bytes")
+                .multiple(true),
+        )
+        .arg(
+            Arg::with_name("delim")
+                .value_name("DELEMITER")
+                .short("d")
+                .long("delim")
+                .help("Field delimiter")
+                .takes_value(true)
+                .default_value("\t")
+                .validator(is_one_byte),
         )
         .get_matches();
 
-    let delimiter = matches.value_of("delimiter").unwrap();
-    let delim_bytes = delimiter.as_bytes();
-    if delim_bytes.len() != 1 {
-        return Err(From::from(format!(
-            "--delim \"{}\" must be a single byte",
-            delimiter
-        )));
-    }
-
-    let fields = matches.value_of("fields").map(parse_pos).transpose()?;
-    let bytes = matches.value_of("bytes").map(parse_pos).transpose()?;
-    let chars = matches.value_of("chars").map(parse_pos).transpose()?;
-
-    let extract = if let Some(field_pos) = fields {
-        Fields(field_pos)
-    } else if let Some(byte_pos) = bytes {
-        Bytes(byte_pos)
-    } else if let Some(char_pos) = chars {
-        Chars(char_pos)
-    } else {
-        return Err(From::from(format!(
-            "the following required arguments were not provided:\n  \
-        <--fields <FIELDS>|--bytes <BYTES>|--chars <CHARS>>"
-        )));
+    let position_list = match parse_pos(
+        matches
+            .value_of_lossy("fields")
+            .map(String::from)
+            .unwrap()
+            .as_str(),
+    ) {
+        Ok(config) => config,
+        Err(e) => {
+            eprint!("{}", e);
+            std::process::exit(1);
+        }
     };
 
     Ok(Config {
         files: matches.values_of_lossy("files").unwrap_or_default(),
-        delimiter: *delim_bytes.first().unwrap(),
-        extract,
+        delimiter: matches
+            .value_of("delim")
+            .map(|c| c.chars().next().unwrap() as u8)
+            .unwrap_or(b','),
+        extract: Fields(position_list),
     })
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    // println!("{:#?}", config);
-    for filename in &config.files {
-        match open(filename) {
-            Err(err) => eprint!("{}: {}", filename, err),
-            Ok(_) => println!("Opened {}", filename),
-        }
-    }
+    println!("{:#?}", config);
     Ok(())
 }
 
-fn parse_pos(range: &str) -> MyResult<PositionList> {
-    let range_re = Regex::new(r"^(\d+)-(\d+)$").unwrap();
-
-    range
-        .split(',')
-        .into_iter()
-        .map(|val| {
-            parse_index(val).map(|n| n..n + 1).or_else(|e| {
-                range_re.captures(val).ok_or(e).and_then(|captures| {
-                    let n1 = parse_index(&captures[1])?;
-                    let n2 = parse_index(&captures[2])?;
-                    if n1 >= n2 {
-                        return Err(format!(
-                            "First number in range ({}) must be lower than second number ({})",
-                            n1 + 1,
-                            n2 + 1
-                        ));
-                    }
-                    Ok(n1..n2 + 1)
-                })
-            })
-        })
-        .collect::<Result<_, _>>()
-        .map_err(From::from)
-}
-
-fn parse_index(input: &str) -> Result<usize, String> {
-    let value_error = || format!("illegal list value: \"{}\"", input);
-
-    input
-        .starts_with("+")
-        .then(|| Err(value_error()))
-        .unwrap_or_else(|| {
-            input
-                .parse::<NonZeroUsize>()
-                .map(|n| usize::from(n) - 1)
-                .map_err(|_| value_error())
-        })
-}
-
-fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
-    match filename {
-        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
-        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+fn is_one_byte(v: String) -> Result<(), String> {
+    if v.len() == 1 && v.as_bytes()[0] <= 8 {
+        Ok(())
+    } else {
+        Err(format!("--delim \"{}\" must be a single byte", v))
     }
 }
 
-fn extract_chars(line: &str, char_pos: &[Range<usize>]) -> String {
-    line.chars()
-        .enumerate()
-        .filter(|(i, _)| char_pos.iter().any(|range| range.contains(i)))
-        .map(|(_, c)| c)
-        .collect()
+fn is_number(val: String) -> Result<(), String> {
+    val.parse::<u32>()
+        .map(|_| ())
+        .map_err(|_| format!("illegal list value: \"{}\"", val))
 }
 
-fn extract_bytes(line: &str, byte_pos: &[Range<usize>]) -> String {
-    // line.as_bytes()
-    unimplemented!();
+fn parse_pos(range: &str) -> MyResult<PositionList> {
+    if range.is_empty() {
+        eprintln!("Empty!");
+        return Err(From::from("Range cannot be empty!"));
+    }
+
+    range
+        .split(',')
+        .map(|part| {
+            if part.contains('-') {
+                convert_to_list(part)
+            } else {
+                let n = validate_element(part);
+                match n {
+                    Ok(0) => Err(From::from(format!("illegal list value: \"0\""))),
+                    Ok(n) => Ok(Range {
+                        start: n - 1,
+                        end: n,
+                    }),
+                    Err(e) => Err(e),
+                }
+            }
+        })
+        .collect::<MyResult<PositionList>>()
+}
+
+fn convert_to_list(v: &str) -> MyResult<Range<usize>> {
+    let mut nums = v.split('-');
+
+    let start = validate_element(nums.next().unwrap());
+    let end = validate_element(nums.next().unwrap());
+
+    if nums.next().is_some() {
+        return Err(From::from(format!("Too many list elements")));
+    }
+
+    match (start, end) {
+        (Ok(0), _) | (_, Ok(0)) => Err(From::from(format!("illegal list value: \"0\""))),
+        (Ok(start), Ok(end)) if start < end => Ok(Range {
+            start: start - 1,
+            end: end,
+        }),
+        (Ok(start), Ok(end)) => Err(From::from(format!(
+            "First number in range ({}) must be lower than second number ({})",
+            start, end
+        ))),
+        (Err(e), _) | (_, Err(e)) => Err(From::from(format!("illegal list value: \"{}\"", v))),
+    }
+}
+
+fn validate_element(v: &str) -> MyResult<usize> {
+    if v.contains("+") {
+        eprintln!("illegal list value: \"{}\"", v);
+        Err(From::from(format!("illegal list value: \"{}\"", v)))
+    } else {
+        let result = v.parse::<usize>();
+        match result {
+            Ok(number) => Ok(number),
+            Err(e) => Err(From::from(format!("illegal list value: \"{}\"", v))),
+        }
+    }
 }
 
 #[cfg(test)]
 mod unit_tests {
-    use super::extract_bytes;
-    use super::extract_chars;
     use super::parse_pos;
 
     #[test]
@@ -292,25 +299,5 @@ mod unit_tests {
         let res = parse_pos("15,19-20");
         assert!(res.is_ok());
         assert_eq!(res.unwrap(), vec![14..15, 18..20]);
-    }
-
-    #[test]
-    fn test_extract_chars() {
-        assert_eq!(extract_chars("", &[0..1]), "".to_string());
-        assert_eq!(extract_chars("ábc", &[0..1]), "á".to_string());
-        assert_eq!(extract_chars("ábc", &[0..1, 2..3]), "ác".to_string());
-        assert_eq!(extract_chars("ábc", &[0..3]), "ábc".to_string());
-        assert_eq!(extract_chars("ábc", &[2..3, 1..2]), "cb".to_string());
-        assert_eq!(extract_chars("ábc", &[0..1, 1..2, 4..5]), "áb".to_string());
-    }
-
-    #[test]
-    fn test_extract_bytes() {
-        assert_eq!(extract_bytes("ábc", &[0..1]), "�".to_string());
-        assert_eq!(extract_bytes("ábc", &[0..2]), "á".to_string());
-        assert_eq!(extract_bytes("ábc", &[0..3]), "áb".to_string());
-        assert_eq!(extract_bytes("ábc", &[0..4]), "ábc".to_string());
-        assert_eq!(extract_bytes("ábc", &[3..4, 2..3]), "cb".to_string());
-        assert_eq!(extract_bytes("ábc", &[0..2, 5..6]), "á".to_string());
     }
 }
